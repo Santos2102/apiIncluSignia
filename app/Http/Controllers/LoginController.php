b8f77e804e165672e46416d011da89c785063907
CCaceres1004 <oscar.caceres2321@gmail.com>
2023-10-09 17:18:43 -0600

    Subiendo configuraciones para consumir API del login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -25,16 +27,36 @@ class LoginController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
-        
+
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $request->session()->regenerate();
-
             return redirect()->intended('dashboard');
         }
 
         return back()->withErrors([
             'email' => 'Las credenciales ingresadas son incorrectas.',
         ]);
+    }
+
+    public function loginMobile(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->email)->get(['id','email','password','roleId'])->first();
+        if($user!=NULL){
+            if(Hash::check($request->password,$user->password)){
+                return response()->json(['message' => 'Password'], 201);
+            }
+            else{
+                return response()->json(['message' => 'No password'], 201);
+            }
+        }
+        else{
+            return response()->json(['message' => 'No email'], 201);
+        }
     }
 
     public function logout(Request $request)
