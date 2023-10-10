c536252e76efcdbe231f59245a9733103038240b
CCaceres1004 <oscar.caceres2321@gmail.com>
2023-10-10 00:04:34 -0600

    Inicio de sesión

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -66,6 +66,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/iniciar-sesión');
+        return redirect('/iniciar-sesion');
     }
 }
