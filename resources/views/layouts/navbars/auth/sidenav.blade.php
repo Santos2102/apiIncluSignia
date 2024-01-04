35eed4d5dae7dd7bc66244750658dcef7950846e
CCaceres1004 <oscar.caceres2321@gmail.com>
2024-01-04 00:09:44 -0600

    Subiendo cambios

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -1,5 +1,5 @@
-<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
-    id="sidenav-main">
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4"
+    id="sidenav-main" style="height: 100vh; overflow-y: auto;">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
@@ -9,8 +9,8 @@
         </a>
     </div>
     <hr class="horizontal dark mt-0">
-    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-        <ul class="navbar-nav">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main" style="height: calc(100% - 56px);">
+        <ul class="navbar-nav" style="height: 100%;">
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}" href="{{ route('home') }}">
                     <div
@@ -116,5 +116,7 @@
             </li>
             @endcan
         </ul>
+    </div>
+    <div class="sidenav-footer mx-3 ">
     </div>
 </aside>
